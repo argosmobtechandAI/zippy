785e5e2ce3eeee579f9174a08320883c0254a507
Uebook <69842121+Uebook@users.noreply.github.com>
2026-06-18 10:38:50 +0530

    Update Android package name to com.zippyEquestrian and configure release signing keystore

--- zipplyRider/android/app/src/main/java/com/zippyEquestrian/MainActivity.kt
+++ zipplyRider/android/app/src/main/java/com/zippyEquestrian/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.zippy
+package com.zippyEquestrian
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- zipplyRider/android/app/src/main/java/com/zippyEquestrian/MainApplication.kt
+++ zipplyRider/android/app/src/main/java/com/zippyEquestrian/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.zippy
+package com.zippyEquestrian
 
 import android.app.Application
 import com.facebook.react.PackageList
